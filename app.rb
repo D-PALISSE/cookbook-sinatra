1cacdad54165088df7ccbb40a3d6e09c1e204a86
David Palisse <palisse.david44@gmail.com>
2019-02-18 17:50:33 +0100

    d

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require_relative 'lib/cookbook'
+require_relative 'lib/recipe'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
+
+New_cookbook = Cookbook.new('lib/recipes.csv')
 
 get '/' do
   erb :home
@@ -13,4 +17,8 @@ end
 
 get '/recipes' do
   erb :recipes
+end
+
+get '/Tiramisu%20aux%20fraises%20(20%20min)' do
+  "Hello World"
 end
